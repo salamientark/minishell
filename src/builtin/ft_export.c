1f8da8e58be03cc66bc48e60ea302b46417c24f6
Pierre Le Guyader <ple-guya@bess-f3r3s5.clusters.42paris.fr>
2024-05-19 19:23:04 +0200

    export sa avance

--- src/builtin/ft_export.c
+++ src/builtin/ft_export.c
@@ -21,44 +21,58 @@ static void	case_noarg(char **env)
 		printf("export %s\n", env[i++]);
 }
 
-static char	*get_name(char *cmd)
+static int	check_valid_var(char *cmd)
 {
-	char	*name;
-	char	*cmd_env;
-	
- 	cmd_env = ft_strchr(cmd, '=');
-	if (!cmd_env)
-		name = cmd;
-	else 
-	{
-		while(cmd[i] != '=')
-		{
-			if 
-			name[i] = cmd[i]
+	int		i;
+	int		len;
+	int		is_equal_sign;
 
+	i = 0;
+	len = 0;
+	is_equal_sign = 0;
+	while (cmd[i])
+	{
+		if (cmd[i] == '=')
+			is_equal_sign = 1;
+		if ((cmd[i] == SINGLE_QUOTE || cmd[i] == DOUBLE_QUOTE) && cmd[i])
+			i++;
+		if ((cmd[i] == '{' || cmd[i] == '}' || cmd[i] == '[' || cmd[i] == ']') 
+			&&  !is_equal_sign)
+		{
+			ft_putendl_fd("invalid name for export", 2);
+			return (0);
 		}
+		len++;
+		i++;
 	}
-	
+	return(len);
 }
 
-static void	add_to_env(char *cmd, char **env)wsl
-
+static char	*get_var(char *cmd, int len)
 {
-	char *name;
 
-	name = get_name(cmd);
+}
 
+static void	add_to_env(char *cmd, char **env)
+{
+	int		len;
+
+	(void)env;
+	len = check_valid_var(cmd);
+	printf("%d\n", len); 
+	if (!len)
+		return ;
+	
 }
 
 void	ft_export(char **cmd, char **env)
 {
 	int i;
 
-	i = 0;
-	if (!cmd[1])
+	i = 1;
+	if (!cmd[i])
 		return (case_noarg(env));
-	while(cmd[i])
+	while(cmd[i + 1])
 		i++;
-	if (i <= 4)
-		add_to_env(cmd[i], env);
+	add_to_env(cmd[i], env);
 }
