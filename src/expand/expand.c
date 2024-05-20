4857bfc2206d2b83de84a3a21cbd3b38ed0c0efc
Jay Moker <dadamien78@gmail.com>
2024-05-20 18:38:39 +0200

    Added tm expand

--- src/expand/expand.c
+++ src/expand/expand.c
@@ -6,11 +6,41 @@
 /*   By: madlab <madlab@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 08:28:07 by madlab            #+#    #+#             */
-/*   Updated: 2024/05/20 13:05:57 by madlab           ###   ########.fr       */
+/*   Updated: 2024/05/20 18:22:26 by madlab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
+
+int	count_expand_word(const char *str)
+{
+	int	word_count;
+	int	index;
+
+	word_count = 1;
+	index = 0;
+	while (str[index])
+	{
+		if (str[index] == DOUBLE_QUOTE || str[index] == SINGLE_QUOTE)
+			index += quoted_strlen(str, index, str[index]);
+		if (str[index] == DOLLAR && str[index + 1]
+			&& !is_whitespace_metachar(str[index + 1]))
+
+		index++;
+	}
+
+}
+
+char	*split_expand(const char *str)
+{
+	int		word_count;
+	char	**expand_tab;
+
+	if (!str)
+		return (NULL);
+	word_count = count_expand_word(str);
+	
+}
 
 int	expand_token(t_token_list *token, char **env)
 {
