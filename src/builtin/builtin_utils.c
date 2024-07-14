379db3c0ea4595265bb6b882cd62caf35bf6f0ef
Pierre Le Guyader <ple-guya@paul-f4Br6s8.clusters.42paris.fr>
2024-07-14 21:53:02 +0200

    almost finish, just one edge case wit echo | cat < infile to fix

--- src/builtin/builtin_utils.c
+++ src/builtin/builtin_utils.c
@@ -33,36 +33,3 @@ int	isbuiltin(char **cmd, t_chill *shell)
 		return (6);
 	return (-1);
 }
-
-static int	tabsize(char **tab)
-{
-	int	len;
-
-	len = 0;
-	while (tab[len])
-		len++;
-	return (len);
-}
-
-char	**update_env(t_chill *shell, char *var)
-{
-	char	**new_env;
-	int		size;
-	int		i;
-
-	i = 0;
-	size = tabsize(shell->env);
-	new_env = (char **)malloc((sizeof(char *) * (size + 2)));
-	if (!new_env)
-		return (NULL);
-	while (shell->env[i])
-	{
-		new_env[i] = shell->env[i];
-		i++;
-	}
-	free(shell->env);
-	shell->env = new_env;
-	shell->env[i] = var;
-	shell->env[i + 1] = NULL;
-	return (new_env);
-}
