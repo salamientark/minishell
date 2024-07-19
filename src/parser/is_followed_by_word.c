d08bafdb7f51dd8b4be93836379936e21c215565
Jay Moker <dadamien78@gmail.com>
2024-07-19 16:34:27 +0200

    Fixed syntax error bug

--- src/parser/is_followed_by_word.c
+++ src/parser/is_followed_by_word.c
@@ -6,7 +6,7 @@
 /*   By: dbaladro <dbaladro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 15:48:06 by dbaladro          #+#    #+#             */
-/*   Updated: 2024/06/05 16:12:09 by dbaladro         ###   ########.fr       */
+/*   Updated: 2024/07/19 16:31:33 by madlab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int	is_followed_by_word(const char *cmd, int operator)
 		operator = LESS_THAN;
 	if (operator == APPEND)
 		operator = GREATER_THAN;
-	if (!cmd[index])
+	if (!cmd[index]
+			||(operator == RIGHT_PARENTHESIS && cmd[index] == operator))
 		return (0);
 	if (cmd[index] && cmd[index] == operator)
 		index++;
@@ -29,8 +30,10 @@ int	is_followed_by_word(const char *cmd, int operator)
 		index++;
 	if (!cmd[index] || cmd[index] == '\n')
 		return (0);
-	if (cmd[index] == GREATER_THAN || cmd[index] == LESS_THAN
-		|| cmd[index] == PIPE)
+	if (get_operator(cmd + index) != 0)
+	// if (((char)operator != ')' && (char)operator != '(') && get_operator(cmd + index) != 0)
+	// if (cmd[index] == GREATER_THAN || cmd[index] == LESS_THAN
+	// 	|| cmd[index] == PIPE || ((char)operator == ')' && cmd[index] == operator))
 		return (0);
 	return (1);
 }
